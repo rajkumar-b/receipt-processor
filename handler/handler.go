9738a9e646082e6dd57b5633afda8ea9e8480f56
Rajkumar Baskar <rajkumarneville@gmail.com>
2024-02-02 14:49:44 -0500

    Add a simple handler to get data from Item model

--- handler/handler.go
+++ handler/handler.go
@@ -2,11 +2,19 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"rajkumar.app/receipt-processor/model"
 )
 
+// sendPing responds with a simple JSON reply.
 func SendPing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+// getItems responds with the list of all items as JSON.
+func GetItems(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, model.Items)
 }
