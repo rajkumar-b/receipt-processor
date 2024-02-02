59b0d947d0ff381935d934f86acde9d376029c6f
Rajkumar <54833237+rajkumar-b@users.noreply.github.com>
2024-02-02 15:12:46 -0500

    Add model (#4)
    * Add a basic item model
    
    * Add comments to model and its variable for global instance storage
    
    * Add a simple handler to get data from Item model
    
    * Add a GET endpoint to list all items
    
    * refactor code

--- handler/handler.go
+++ handler/handler.go
@@ -2,11 +2,19 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"rajkumar.app/receipt-processor/model"
 )
 
+// SendPing responds with a simple JSON reply.
 func SendPing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+// GetItems responds with the list of all items as JSON.
+func GetItems(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, model.Items)
 }
--- main.go
+++ main.go
@@ -14,10 +14,11 @@ func main() {
 
 	// Add routes
 	router.GET("/ping", handler.SendPing)
+	router.GET("/items", handler.GetItems)
 
 	// Start server
 	port := 8080
 	fmt.Printf("\nReceipt Processor Server is running on port %d...\n", port)
-	fmt.Printf("Access the API via localhost: http://localhost:%d/ping\n\n", port)
+	fmt.Printf("Access the API via localhost: http://localhost:%d/<endpoint>\n\n", port)
 	router.Run(fmt.Sprintf("localhost:%d", port))
 }
