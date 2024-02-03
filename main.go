9c592c3b400d97417a2cc19aba958edfd44d641e
Rajkumar Baskar <rajkumarneville@gmail.com>
2024-02-03 00:15:58 -0500

    Split server logic from main and move to server directory

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"rajkumar.app/receipt-processor/handler"
+	"rajkumar.app/receipt-processor/server"
 )
 
 func main() {
-	// Configure router
-	router := gin.Default()
-	router.ForwardedByClientIP = true
-	router.SetTrustedProxies([]string{"127.0.0.1"})
-
-	// Add routes
-	router.GET("/ping", handler.SendPing)
-	router.GET("/items", handler.GetItems)
-
-	// Start server and bind to all network interfaces
 	port := 8080
-	fmt.Printf("\nReceipt Processor Server is running on port %d...\n", port)
-	fmt.Printf("Access the API via localhost: http://localhost:%d/<endpoint>\n\n", port)
-	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), router)
+	err := server.StartServer(port)
 	if err != nil {
 		fmt.Println("Failed to start the server:", err)
 	}
---
+++ server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"rajkumar.app/receipt-processor/handler"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.ForwardedByClientIP = true
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.GET("/ping", handler.SendPing)
+	router.GET("/items", handler.GetItems)
+
+	return router
+}
+
+func StartServer(port int) error {
+	router := setupRouter()
+
+	fmt.Printf("\nReceipt Processor Server is running on port %d...\n", port)
+	fmt.Printf("Access the API via localhost: http://localhost:%d/<endpoint>\n\n", port)
+
+	return http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), router)
+}
