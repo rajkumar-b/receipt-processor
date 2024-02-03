d10695b6e3bc071bbc27833007b26a70b670b884
Rajkumar Baskar <rajkumarneville@gmail.com>
2024-02-03 17:11:38 -0500

    Add additional test cases

--- model/item_test.go
+++ model/item_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +11,37 @@ func TestItemValidation(t *testing.T) {
 	t.Run("Valid Item", func(t *testing.T) {
 		validItem := Item{
 			Description: "Valid Description",
-			Price:       10.99,
+			Price:       "10.99",
 		}
 
 		err := validItem.Validate()
 		assert.NoError(t, err, "Validation should pass for a valid item")
 	})
 
-	t.Run("Invalid Description", func(t *testing.T) {
+	t.Run("Valid Description - Underscore", func(t *testing.T) {
+		validItem := Item{
+			Description: "Valid_Description",
+			Price:       "2.99",
+		}
+
+		err := validItem.Validate()
+		assert.NoError(t, err, "Validation should pass for a valid item")
+	})
+
+	t.Run("Valid Description - Numbers", func(t *testing.T) {
+		validItem := Item{
+			Description: "Val1d D3scr1pt10n",
+			Price:       "6.00",
+		}
+
+		err := validItem.Validate()
+		assert.NoError(t, err, "Validation should pass for a valid item")
+	})
+
+	t.Run("Invalid Description - Symbols", func(t *testing.T) {
 		invalidItem := Item{
-			Description: "Invalid-Description!@#$",
-			Price:       10.99,
+			Description: "Invalid!@#$Description",
+			Price:       "10.99",
 		}
 
 		err := invalidItem.Validate()
@@ -28,15 +49,75 @@ func TestItemValidation(t *testing.T) {
 		assert.Contains(t, err.Error(), "descriptionValidator", "Error message should mention the failed validator")
 	})
 
-	t.Run("Invalid Price", func(t *testing.T) {
+	t.Run("Valid Price - Zeroes", func(t *testing.T) {
+		validItem := Item{
+			Description: "Val1d D3scr1pt10n",
+			Price:       "0.00",
+		}
+
+		err := validItem.Validate()
+		assert.NoError(t, err, "Validation should pass for a valid item")
+	})
+
+	t.Run("Invalid Price - Integer as string", func(t *testing.T) {
 		invalidItem := Item{
 			Description: "Valid Description",
-			Price:       -5.0,
+			Price:       "5",
 		}
 
 		err := invalidItem.Validate()
 		assert.Error(t, err, "Validation should fail for an invalid price")
-		assert.Contains(t, err.Error(), "gte", "Error message should mention the failed validator")
+		assert.Contains(t, err.Error(), "priceValidator", "Error message should mention the failed validator")
+	})
+
+	t.Run("Invalid Price - Single Decimal", func(t *testing.T) {
+		invalidItem := Item{
+			Description: "Valid Description",
+			Price:       "5.0",
+		}
+
+		err := invalidItem.Validate()
+		assert.Error(t, err, "Validation should fail for an invalid price")
+		assert.Contains(t, err.Error(), "priceValidator", "Error message should mention the failed validator")
+	})
+
+	t.Run("Invalid Price - Negative Number", func(t *testing.T) {
+		invalidItem := Item{
+			Description: "Valid Description",
+			Price:       "-5.00",
+		}
+
+		err := invalidItem.Validate()
+		assert.Error(t, err, "Validation should fail for an invalid price")
+		assert.Contains(t, err.Error(), "priceValidator", "Error message should mention the failed validator")
+	})
+
+	t.Run("Invalid Price - Integer as int", func(t *testing.T) {
+		invalidJSON := `{
+			"shortDescription": "Valid Description",
+			"price": 5
+		}`
+	
+		var newItem Item
+	
+		err := json.Unmarshal([]byte(invalidJSON), &newItem)
+	
+		assert.Error(t, err, "Binding should fail for an invalid price")
+		assert.Contains(t, err.Error(), "json", "Error message should mention JSON decoding error")
+	})
+
+	t.Run("Invalid Price - Decimal as float", func(t *testing.T) {
+		invalidJSON := `{
+			"shortDescription": "Valid Description",
+			"price": 5.99
+		}`
+	
+		var newItem Item
+	
+		err := json.Unmarshal([]byte(invalidJSON), &newItem)
+	
+		assert.Error(t, err, "Binding should fail for an invalid price")
+		assert.Contains(t, err.Error(), "json", "Error message should mention JSON decoding error")
 	})
 }
 
@@ -50,8 +131,8 @@ func TestValidateItems(t *testing.T) {
 	t.Run("Valid Items", func(t *testing.T) {
 		// Add some valid items to the Items slice
 		validItems := []Item{
-			{Description: "Item1", Price: 10.99},
-			{Description: "Item2", Price: 5.0},
+			{Description: "Item1", Price: "10.99"},
+			{Description: "Item2", Price: "5.00"},
 		}
 
 		Items = append(Items, validItems...)
@@ -63,8 +144,8 @@ func TestValidateItems(t *testing.T) {
 	t.Run("Invalid Items", func(t *testing.T) {
 		// Add some invalid items to the Items slice
 		invalidItems := []Item{
-			{Description: "Invalid-Item!@#", Price: -5.0},
-			{Description: "Item3", Price: 8.99},
+			{Description: "Invalid-Item!@#", Price: "5.00"},
+			{Description: "Item3", Price: "8.99"},
 		}
 
 		Items = append(Items, invalidItems...)
